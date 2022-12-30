58c1409caa228f3e1e76ad392d85c180cec751e8
Cansın <ayurekl97@gmail.com>
2022-12-30 15:04:12 +0300

    Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import Chat from "./chat/Chat";
 import { ChatContextProvider } from "./store/context/ChatContext";
-import Bar from "./bar/Bar";
+import { UserContextProvider } from "./store/context/UserContext";
+import { Route, Routes } from "react-router-dom";
+import Login from "./login/Login";
 function App() {
   return (
-    <ChatContextProvider>
-      <div className="App">
-        <Bar />
-        <Chat />
-      </div>
-    </ChatContextProvider>
+    <div className="App">
+      <UserContextProvider>
+        <ChatContextProvider>
+          <div className="Container">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/chat" element={<Chat />} />
+            </Routes>
+          </div>
+        </ChatContextProvider>
+      </UserContextProvider>
+    </div>
   );
 }
 
