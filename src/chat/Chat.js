12e6b61f534b85ea1af3640739b8628f14b21907
Cansın <ayurekl97@gmail.com>
2022-12-30 15:27:34 +0300

    Chat

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,21 +1,48 @@
-import React, { useContext } from "react";
-import { ChatContext } from "../store/context/ChatContext";
-import Input from "../ınput/Input";
 import uuid from "react-uuid";
-
+import Input from "../ınput/Input";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ChatContext } from "../store/context/ChatContext";
+import "./Index.css";
+import Ahmet from "./image/01.jpeg";
+import Mehmet from "./image/02.jpeg";
 const Chat = () => {
+  const navigate = useNavigate();
   const [chat, setChat] = useContext(ChatContext);
 
   const sendMessage = (chat) => {
-    setChat([...chat, { id: uuid(), chat: chat }]);
+    setChat((prev) => {
+      return [...prev, { id: uuid(), chat: chat }];
+    });
+  };
+  const exit = () => {
+    navigate("/");
   };
   return (
-    <div>
-      {chat.map((chat) => (
-        <div key={chat.id}>{chat.chat}</div>
-      ))}
-      <Input sendMessage={sendMessage} />
-    </div>
+    <>
+      <div className="container">
+        {chat.map((chat) => (
+          <div key={chat.id}>
+            <img src={Ahmet} alt="Avatar" className="Images" />
+            {chat.sendTheMessage}
+            {chat.chat}
+          </div>
+        ))}
+      </div>
+      <div className="darker">
+        {chat.map((chat) => (
+          <div key={chat.id}>
+            <img src={Mehmet} alt="Avatar" className="Images" />
+            {chat.sendTheMessage}
+            {chat.chat}
+          </div>
+        ))}
+      </div>
+      <Input sendMessage={sendMessage} className="ChatInput" />
+      <button onClick={exit} className="ExitButton">
+        Exit
+      </button>
+    </>
   );
 };
 
