c8d0860d571bcf4593a408917fce8c7d59db7765
Cansın <ayurekl97@gmail.com>
2023-01-12 17:45:53 +0300

    not emy input set

--- src/ınput/Input.js
+++ src/ınput/Input.js
@@ -1,54 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useStatus } from "../store/context/StatusContext";
 import "./Index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faMicrophone,
-  faCamera,
-  faPaperclip,
-  faPaperPlane,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 const Input = ({ click }) => {
   const { sendValue, setSendValue } = useStatus();
-  const [isWritingIcon, setIsWritingIcon] = useState();
-  const onChange = (e) => {
-    setSendValue(e.target.value);
-  };
-
-  useEffect(() => {
-    if (sendValue?.length > 0) {
-      setIsWritingIcon(
-        <FontAwesomeIcon
-          type="submit"
-          onClick={click}
-          icon={faPaperPlane}
-          size="lg"
-        />
-      );
-    } else {
-      setIsWritingIcon(
-        <FontAwesomeIcon
-          type="submit"
-          onClick={click}
-          icon={faMicrophone}
-          size="lg"
-        />
-      );
-    }
-  }, [sendValue]);
 
   return (
     <div className="buttonIn">
       <input
         className="ChatInput"
         type="text"
-        onChange={onChange}
+        onChange={(event) => setSendValue(event.target.value)}
         maxLength="64"
         placeholder="Mesaj"
         value={sendValue}
       />
-      <button className="Faicon">{isWritingIcon}</button>
+      <button
+        type="submit"
+        disabled={!sendValue}
+        className="Faicon"
+        onClick={click}
+      >
+        <FontAwesomeIcon
+          icon={sendValue?.length > 0 ? faPaperPlane : faMicrophone}
+          size="lg"
+        />
+      </button>
     </div>
   );
 };
